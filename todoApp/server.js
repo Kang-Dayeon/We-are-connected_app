edf3d6b56a48271b44b62b570a8bf985876d3cbd
kangdayeon <dykang@ui-lab.co.kr>
2022-06-29 14:11:49 +0900

    로그인기능 만들어보기

--- todoApp/server.js
+++ todoApp/server.js
@@ -76,7 +76,6 @@ app.get('/write', function (요청, 응답) {
 // });
 
 //DB저장 방법
-
 app.post('/write', function (요청, 응답) {
   db.collection('counter').findOne({ name: '게시물갯수' }, function (에러, 결과) {
     // console.log(결과.totalPost);
@@ -133,3 +132,40 @@ app.put('/edit', function (요청, 응답) {
     응답.redirect('/list');
   })
 })
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const session = require('express-session')
+
+app.use(session({ secret: '비밀코드', resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', function (요청, 응답) {
+  응답.render('login.ejs')
+});
+
+app.post('/login', passport.authenticate('local', {
+  failureRedirect: '/fail'
+}), function (요청, 응답) {
+  응답.redirect('/')
+});
+
+passport.use(new LocalStrategy({
+  usernameField: 'id',
+  passwordField: 'pw',
+  session: true,
+  passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+  //console.log(입력한아이디, 입력한비번);
+  db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+    if (에러) return done(에러)
+
+    if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+    if (입력한비번 == 결과.pw) {
+      return done(null, 결과)
+    } else {
+      return done(null, false, { message: '비번틀렸어요' })
+    }
+  })
+}));
